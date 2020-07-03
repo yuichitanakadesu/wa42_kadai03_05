6abfd790a13ccf669864a90c3df3b54d427dba85
YuichiTanaka <osmanthus3720@gmail.com>
2020-07-03 16:33:56 +0900

    nuxt

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>sample</h1>
+    <GmapMap
+      :center="{lat:10, lng:10}"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px;"
+    />
+  </div>
+</template>
+
+<style scoped>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  color: #526488;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
